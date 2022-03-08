9121afaf9b0e3605b0b3efdee7a3de28e5b14727
filippo.ricciotti0@gmail.com <filippo.ricciotti0@gmail.com>
2022-03-08 17:41:41 +0100

    aggiunto sun

--- boh/Program.cs
+++ boh/Program.cs
@@ -11,7 +11,7 @@ namespace boh
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World and sun");
         }
     }
 }
